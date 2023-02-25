73bed8e8dd678bab355cff888431d5b097f83e90
iamfutureflash <xenode2000@gmail.com>
2023-02-25 19:28:02 +0530

    changed text

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Blog() {
     return (
-        <div>blog</div>
+        <div>This is the blog</div>
     )
 }
 
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Contact() {
     return (
-        <div>contact</div>
+        <div>This is the contact</div>
     )
 }
 
